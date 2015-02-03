4690b08a0b3a7541e9a34246fdf22a0c3f6edb5a
Dave Webb <wfh@dcw.co.nz>
2015-02-04 09:12:48 +1300

    DCW - Switch to DateTime::Format::DateParse, as that works for Tue Feb 03 23:46:04 GMT+1300 2015

--- date.pl
+++ date.pl
@@ -3,9 +3,17 @@
 use warnings;
 use strict;
 
-use DateTime::Format::Flexible;
+use DateTime::Format::DateParse;
 
 my $date_str = join ' ', @ARGV;
 
 print "FROM: $date_str\n";
+
+my $dt = DateTime::Format::DateParse->parse_datetime($date_str);
+
+print "PARSED: $dt\n";
+
+$dt->set_time_zone('GMT');
+
+print "GMT: $dt\n";
 
